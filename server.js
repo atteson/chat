08e8afad0a486f011f69c1fd646db8e9994a231d
BuildTools <unconfigured@null.spigotmc.org>
2022-06-07 19:28:12 -0400

    de-lambdad

--- server.js
+++ server.js
@@ -6,39 +6,19 @@ const port = 6969;
 const server = http.createServer(express);
 const wss = new WebSocket.Server({ server })
 
-//server.listen(port, function() {
-//  console.log(`Server is listening on ${port}!`)
-//})
-
-//function onMessage(data) {
-//    for(var client in wss.clients) {
-//	if (client.readyState === WebSocket.OPEN) {
-//	    client.send(data);
-//	}
-//    }
-//}
+function onMessage(data) {
+    for(const client of wss.clients) {
+	if (client.readyState === WebSocket.OPEN) {
+	    client.send(data);
+	}
+    }
+}
 
 function onConnection(ws) {
-  ws.on('message', function incoming(data) {
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
-    })
-  })
+    ws.on('message', onMessage);
 }
 
 wss.on('connection', onConnection );
-
-//wss.on('connection', function connection(ws) {
-//  ws.on('message', function incoming(data) {
-//    wss.clients.forEach(function each(client) {
-//      if (client.readyState === WebSocket.OPEN) {
-//        client.send(data);
-//      }
-//    })
-//  })
-//})
 
 function onListen() {
   console.log(`Server is listening on ${port}!`)
