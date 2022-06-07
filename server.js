aa78c356c5abe81e7a3311ccbf17d28b2b3f7d18
BuildTools <unconfigured@null.spigotmc.org>
2022-06-07 19:16:28 -0400

    second change

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const wss = new WebSocket.Server({ server })
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(data) {
     wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
+      if (client.readyState === WebSocket.OPEN) {
         client.send(data);
       }
     })
