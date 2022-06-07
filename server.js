a3de00d27712cc2c4705b7d7f92a4266b1c6410e
BuildTools <unconfigured@null.spigotmc.org>
2022-06-07 19:18:38 -0400

    third change

--- server.js
+++ server.js
@@ -6,16 +6,6 @@ const port = 6969;
 const server = http.createServer(express);
 const wss = new WebSocket.Server({ server })
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(data) {
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
-    })
-  })
-})
-
 //server.listen(port, function() {
 //  console.log(`Server is listening on ${port}!`)
 //})
@@ -28,11 +18,27 @@ wss.on('connection', function connection(ws) {
 //    }
 //}
 
-//function onConnection(ws) {
-//    ws.on('message', onMessage );
-//}
+function onConnection(ws) {
+  ws.on('message', function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    })
+  })
+}
 
-//wss.on('connection', onConnection );
+wss.on('connection', onConnection );
+
+//wss.on('connection', function connection(ws) {
+//  ws.on('message', function incoming(data) {
+//    wss.clients.forEach(function each(client) {
+//      if (client.readyState === WebSocket.OPEN) {
+//        client.send(data);
+//      }
+//    })
+//  })
+//})
 
 function onListen() {
   console.log(`Server is listening on ${port}!`)
